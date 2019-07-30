5d7752c24a9dfe355b025780e191095260755a1e
= <=>
2019-07-30 20:13:04 +0530

    UI build

---
+++ build/precache-manifest.291a1b16feaf8b1fc33065f9fca728b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5457b1a0e162fec1d400865f249fbefd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d9362045abe646d914b",
+    "url": "/static/css/main.1571ea2a.chunk.css"
+  },
+  {
+    "revision": "d3e9d32a5bb8cb91c42c",
+    "url": "/static/js/2.1cd16726.chunk.js"
+  },
+  {
+    "revision": "2d9362045abe646d914b",
+    "url": "/static/js/main.6d890245.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
